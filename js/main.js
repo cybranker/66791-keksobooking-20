09789c5c0fa20898dd7988f12c6ae4a3c0ef5339
cybranker <cybranker@yandex.ru>
2020-06-05 16:46:31 +0300

    Добавляет отображение похожих обьявлений на карте

--- js/main.js
+++ js/main.js
@@ -84,3 +84,32 @@ var getArrayAds = function (quantity) {
 
   return arr;
 };
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+var mapPinTemplate = document.querySelector('#pin')
+  .content
+  .querySelector('.map__pin');
+
+var mapPinsListElement = document.querySelector('.map__pins');
+
+var getFragmentMapPins = function (ads) {
+  var fragment = document.createDocumentFragment();
+  var mapPinElement;
+
+  for (var i = 0; i < ads.length; i++) {
+    mapPinElement = mapPinTemplate.cloneNode(true);
+
+    mapPinElement.style.left = ads[i].location.x + 'px';
+    mapPinElement.style.top = ads[i].location.y + 'px';
+    mapPinElement.style.transform = 'translate(-50%, -100%)';
+    mapPinElement.querySelector('img').src = ads[i].author.avatar;
+    mapPinElement.querySelector('img').alt = ads[i].offer.title;
+
+    fragment.appendChild(mapPinElement);
+  }
+
+  return fragment;
+};
+
+mapPinsListElement.appendChild(getFragmentMapPins(getArrayAds(QUANTITY_ADS)));
